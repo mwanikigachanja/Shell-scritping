b6437f6b5577c4d9977711585115847c2529e999
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-04-13 15:25:08 +0300

    Can execute the file
    Simply enter ./backup.sh and run the script

--- backups.sh
+++ backups.sh
@@ -1,4 +1,14 @@
-#!/bin/bash                                                                                                           
+
+#touch backup.sh
+
+#sudo chmod 755 backup.sh
+#nano backup.sh 
+
+#!/bin/bash   
+
+
 DAY="$(date +%Y_%m_%d)"                                                       
 BACKUP="/home/$USER/backups/$DAY-backup-CompanyA.tar.gz"                             
 tar -csvpzf $BACKUP /home/$USER/CompanyA
+
+//Run the script: ./backup.sh
